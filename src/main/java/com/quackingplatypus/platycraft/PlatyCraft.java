b38920e77d192467156a2b8a4764b98484262032
Quackingplatypus <minecraftplatypus@gmail.com>
2021-02-14 00:14:05 -0500

    Added Mod Class Skeleton

---
+++ src/main/java/com/quackingplatypus/platycraft/PlatyCraft.java
@@ -0,0 +1,32 @@
+package com.quackingplatypus.platycraft;
+
+//MinecraftForge Classes
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = "PlatyCraft", name = "PlatyCraft", version = "1.7.10-1.0")
+public class PlatyCraft
+{
+    @Mod.Instance("PlatyCraft")
+    public static PlatyCraft instance;
+
+    @Mod.EventHandler
+    public void preInitialization(FMLPreInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void initialization(FMLInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void postInitialization(FMLPostInitializationEvent event)
+    {
+
+    }
+}
